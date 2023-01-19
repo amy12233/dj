fc6967c21fa7990c1037e5f839c0e77fdda5c055
amy <93597699+amy12233@users.noreply.github.com>
2023-01-19 10:46:42 -0600

    Add files via upload
    yes

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 song1="";
 song2="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw() {
-    Image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 600, 500);
 }
 function preload() {
     song1 = loadSound("music.mp3");
@@ -16,4 +23,23 @@ function preload() {
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded () {
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX = " + leftWristX +" rightWristY = "+ rightWristY);
+    }
 }
