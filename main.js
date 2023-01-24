116a129d7529b4676d6b0f45cd7027784a916de1
amy <93597699+amy12233@users.noreply.github.com>
2023-01-24 09:26:47 -0600

    Add files via upload
    yes

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+song1_status = "";
+song2_status = "";
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
@@ -15,6 +18,23 @@ function setup() {
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+     fill("#FF0000");
+     stroke()
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY,20);
+        
+        song1.stop()
+        if(song2_status = false) 
+        {
+            song2.play();
+
+            document.getElementById("song1").innerHTML ="song1 = " +song1;
+
+        }
+
+    }
 }
 function preload() {
     song1 = loadSound("music.mp3");
@@ -34,6 +54,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
